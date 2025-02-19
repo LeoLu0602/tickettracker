431ba9630f4e0f2e966962235ad811404c3d9547
Leo Lu <lyuholu@gmail.com>
2025-02-18 22:50:22 -0500

    feat: allowe users to schedule cron jobs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import cron from "node-cron";
 
 const app = express();
 const port = 8080;
@@ -13,48 +14,49 @@ puppeteer.use(StealthPlugin());
 
 /**
  * @param {string} url
+ * @param {number} quantity
  * @return {Promise<string>}
  */
-async function startTracking(url, quantity) {
-    console.log("Launching browser...");
-
+async function getLowestPrice(url, quantity) {
     const browser = await puppeteer.launch({ headless: true });
-
-    console.log("Creating new page...");
-
     const page = await browser.newPage();
     
     // Set a realistic user-agent to avoid detection.
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
 
     // Navigate the page to a URL.
-    console.log(`Navigating to ${url}...`);
     await page.goto(url);
-    console.log("Waiting for selector #filter-bar-quantity...");
     await page.waitForSelector("#filter-bar-quantity");
-    console.log(`Select ${quantity} ticket${quantity > 1 ? "s" : ""}`);
     await page.select("#filter-bar-quantity", quantity.toString());
 
     // Locate the lowest price.
-    console.log("Waiting for the selector #quickpick-buy-button-qp-0...");
     const lowestPriceSelector = await page.waitForSelector("#quickpick-buy-button-qp-0");
     const lowestPrice = await lowestPriceSelector?.evaluate(el => el.textContent) ?? "";
-
-    // Print the lowest price.
-    if (lowestPrice !== "") {
-        console.log(`The lowest price for ${quantity} ticket${quantity > 1 ? "s" : ""} is ${lowestPrice}`,);
-    } else {
-        console.log("Oops! Something went wrong.");
-    }
 
     return lowestPrice;
 }
 
+/**
+ * @param {string} url
+ * @param {number} quantity
+ * @return {Promise<void>}
+ */
+function startTracking(url, quantity) {
+    cron.schedule("0 * * * * *", async () => {
+        const lowestPrice = await getLowestPrice(url, quantity);
+
+        console.log(`[${new Date().toLocaleString()}] ${quantity} ticket${quantity > 1 ? "s" : ""} ${lowestPrice}`);
+    });
+}
+
 app.post("/api/v1/trackers", async (req, res) => {
     const { url, quantity } = req.body;
-    const lowestPrice = await startTracking(url, quantity);
 
-    res.json({ lowestPrice });
+    startTracking(url, quantity);
+
+    res.json({
+        message: "Your tracker was created successfully!"
+    });
 });
 
 app.listen(port, () => {
